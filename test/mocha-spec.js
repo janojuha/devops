187e5c5809ce93a41afeb88b4f0cebfa48e38d39
Janos Juhasz <janos.juhasz@hu.ibm.com>
2016-03-07 18:11:35 +0100

    changed unit tests to gulp-mocha

--- test/mocha-spec.js
+++ test/mocha-spec.js
@@ -1,25 +1,25 @@
-var request = require("request"),
-    assert = require('assert'),
-    appUnderTest = require("../hello.js");
+var rewire = require('rewire');
+var chai = require('chai');
+var expect = chai.expect;
 var cfenv = require('cfenv');
 var appEnv = cfenv.getAppEnv();
-var   base_url = "http://localhost:"+appEnv.port;
-	
+var base_url = "http://localhost:"+appEnv.port;
+var request = require("request");
 
-
+chai.config.includeStack = false;
 
 describe("/ URL responses without error targeting port: " +appEnv.port , function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
-        assert.equal(200, response.statusCode);
+		expect(response.statusCode).to.be.equal(200);
         done();
       });
     });
 
     it("returns expected test", function(done) {
       request.get(base_url, function(error, response, body) {
-        assert.equal("<html><head></head><body><h1>Test ok</h1></body></html>", body);
+		expect(body).to.be.equal("<html><head></head><body><h1>Test ok</h1></body></html>");
         done();
       });
     });
